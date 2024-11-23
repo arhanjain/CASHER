8a4231f60b2b0cc499b5380333f8082592ea81f8
Arhan Jain <arhan.j04@gmail.com>
2024-11-23 14:29:01 -0800

    first casher website, with viz

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
+    react(),
+  ],
   base: '/CASHER/'
 })
